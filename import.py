badcb2784772b7aaf482b84ccb42e2c5583af0eb
Soichi Hayashi <hayashis@iu.edu>
2016-01-08 19:29:49 +0000

    minor fix for skelton

--- import.py
+++ import.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import logging
-logging.basicConfig(filename='run.log',level=logging.DEBUG)
+#logging.basicConfig(filename='run.log',level=logging.DEBUG)
 #logging.debug('This message should go to the log file')
 #logging.info('So should this')
 #logging.warning('And this, too')
@@ -16,7 +16,7 @@ config=json.loads(config_json)
 
 #TODO..
 
-product = {'type': config.type, 'whaterver': 'test'}
+product = {'type': config["type"], 'whaterver': 'test'}
 with open('products.json', 'w') as out:
     json.dump([product], out)
 
---
+++ test/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+export SCA_SERVICE_DIR=~/git/sca-service-upload
+../import.py
